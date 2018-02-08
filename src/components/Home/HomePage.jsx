a781cc18972b4d3451259b2053bfa66a2343167e
jward1701d <jward1701d@gmail.com>
2018-02-08 10:03:47 -0500

    Add main page image and imported it. Darkend the film of the background another 10%.

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './homepage.css';
 
+const photo = require('./selfie1.JPG');
+
+
 class HomePage extends Component {
   render() {
     return (
@@ -8,10 +11,10 @@ class HomePage extends Component {
         <div className="left">
           <h1>James  Ward</h1>
           <h3>- Web developer</h3>
-          <h3>- Game Programmer</h3>
+          <h3>- Game Designer / Programmer</h3>
         </div>
         <div className="right">
-          <img src="http://via.placeholder.com/250x250"/>
+          <img src={photo}/>
         </div>
       </div>
     );
